d30f50722ad6aee51e2ec08ac709fdbd5d154cf8
quh4gko8 <quh4gko8@noreplies.github.io>
2022-05-08 06:02:25 +0000

    Support disabling backup app

--- src/com/android/settings/backup/BackupSettingsPreferenceController.java
+++ src/com/android/settings/backup/BackupSettingsPreferenceController.java
@@ -16,20 +16,33 @@
 
 package com.android.settings.backup;
 
+import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Process;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
 import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.widget.PrimarySwitchPreference;
 import com.android.settingslib.core.AbstractPreferenceController;
 
 public class BackupSettingsPreferenceController extends AbstractPreferenceController
-        implements PreferenceControllerMixin {
+        implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
     private static final String BACKUP_SETTINGS = "backup_settings";
-    private static final  String MANUFACTURER_SETTINGS = "manufacturer_backup";
+    private static final String MANUFACTURER_SETTINGS = "manufacturer_backup";
+
+    private final PackageManager packageManager;
+    private final String backupPkgName;
+    private final int userId;
+
+    private PrimarySwitchPreference mBackupSettings;
+    private Preference mManufacturerSettings;
     private Intent mBackupSettingsIntent;
     private CharSequence mBackupSettingsTitle;
     private String mBackupSettingsSummary;
@@ -44,17 +57,51 @@ public class BackupSettingsPreferenceController extends AbstractPreferenceContro
         mBackupSettingsSummary = settingsHelper.getSummaryForBackupSettings();
         mManufacturerIntent = settingsHelper.getIntentProvidedByManufacturer();
         mManufacturerLabel = settingsHelper.getLabelProvidedByManufacturer();
+        packageManager = context.getPackageManager();
+        backupPkgName = mBackupSettingsIntent.getComponent().getPackageName();
+	userId = context.getUserId();
     }
 
     @Override
     public void displayPreference(PreferenceScreen screen) {
-        Preference backupSettings = screen.findPreference(BACKUP_SETTINGS);
-        Preference manufacturerSettings = screen.findPreference(MANUFACTURER_SETTINGS);
-        backupSettings.setIntent(mBackupSettingsIntent);
-        backupSettings.setTitle(mBackupSettingsTitle);
-        backupSettings.setSummary(mBackupSettingsSummary);
-        manufacturerSettings.setIntent(mManufacturerIntent);
-        manufacturerSettings.setTitle(mManufacturerLabel);
+        mBackupSettings = screen.findPreference(BACKUP_SETTINGS);
+        mBackupSettings.setChecked(isBackupAppEnabled());
+        mManufacturerSettings = screen.findPreference(MANUFACTURER_SETTINGS);
+        mBackupSettings.setIntent(mBackupSettingsIntent);
+        mBackupSettings.setTitle(mBackupSettingsTitle);
+        mBackupSettings.setSummary(mBackupSettingsSummary);
+        mManufacturerSettings.setIntent(mManufacturerIntent);
+        mManufacturerSettings.setTitle(mManufacturerLabel);
+    }
+
+    private boolean isBackupAppEnabled() {
+        try {
+            int state = packageManager.getApplicationEnabledSetting(backupPkgName);
+            if (state != COMPONENT_ENABLED_STATE_ENABLED) {
+                return false;
+            }
+            return true;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String key = preference.getKey();
+        if (BACKUP_SETTINGS.equals(key)) {
+            boolean newChecked = !isBackupAppEnabled();
+            int state = newChecked ? COMPONENT_ENABLED_STATE_ENABLED : COMPONENT_ENABLED_STATE_DISABLED;
+            try {
+                packageManager.setApplicationEnabledSetting(backupPkgName, state, userId);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
     }
 
     /**
