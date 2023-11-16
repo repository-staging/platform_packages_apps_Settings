2cd4c843d13620fdbbe024914502ed336315d8d8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-16 17:23:47 +0200

    add per-app setting for hardened_malloc

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -539,5 +539,7 @@ public class Settings extends SettingsActivity {
     public static class MagnificationActivity extends SettingsActivity { /* empty */ }
     public static class FlashNotificationsActivity extends SettingsActivity { /* empty */ }
 
+    public static class AppHardenedMallocActivity extends SettingsActivity {}
+
     public static class ExploitProtectionActivity extends SettingsActivity {}
 }
