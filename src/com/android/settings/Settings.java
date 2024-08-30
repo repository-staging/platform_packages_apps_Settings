64d09212287e87ae0ea4f65ccd0f53d26f86d656
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-08-30 16:25:59 +0300

    add per-app setting for memory tagging

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -539,6 +539,8 @@ public class Settings extends SettingsActivity {
     public static class MagnificationActivity extends SettingsActivity { /* empty */ }
     public static class FlashNotificationsActivity extends SettingsActivity { /* empty */ }
 
+    public static class AppMemtagActivity extends SettingsActivity {}
+
     public static class AppHardenedMallocActivity extends SettingsActivity {}
 
     public static class AppNativeDebuggingActivity extends SettingsActivity {}
