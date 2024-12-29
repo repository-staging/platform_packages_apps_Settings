14627bded2a482a4704a1d925b478ef4029a5fa9
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-12-29 19:34:13 +0200

    add battery charge limit setting

--- src/com/google/android/settings/overlay/FeatureFactoryGoogleImpl.kt
+++ src/com/google/android/settings/overlay/FeatureFactoryGoogleImpl.kt
@@ -1,6 +1,16 @@
 package com.google.android.settings.overlay
 
+import com.google.android.settings.fuelgauge.BatterySettingsFeatureProviderGoogleImpl
 import com.android.settings.overlay.FeatureFactoryImpl
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl
 
 class FeatureFactoryGoogleImpl : FeatureFactoryImpl() {
+
+    override val powerUsageFeatureProvider by lazy {
+        PowerUsageFeatureProviderGoogleImpl(appContext)
+    }
+
+    override val batterySettingsFeatureProvider by lazy {
+        BatterySettingsFeatureProviderGoogleImpl()
+    }
 }
