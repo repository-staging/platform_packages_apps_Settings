12e22b0fab08e87f3e095ebfb2978d10795fb61b
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-01-25 12:01:35 +0200

    Revert "add missing face unlock strings for English"
    Face Unlock is currently disabled on all supported devices. If it gets enabled, then these strings
    should be extracted automatically via adevtool from TrafficLightFaceOverlay.apk

--- src/com/android/settings/biometrics/face/FaceEnrollIntroduction.java
+++ src/com/android/settings/biometrics/face/FaceEnrollIntroduction.java
@@ -365,34 +365,34 @@ public class FaceEnrollIntroduction extends BiometricEnrollIntroduction {
 
     @StringRes
     protected int getInfoMessageGlasses() {
-        return R.string.security_settings_face_enroll_introduction_info_glasses_en;
+        return R.string.security_settings_face_enroll_introduction_info_glasses;
     }
 
     @StringRes
     protected int getInfoMessageLooking() {
         return isPrivateProfile()
                 ? R.string.private_space_face_enroll_introduction_info_looking
-                : R.string.security_settings_face_enroll_introduction_info_looking_en;
+                : R.string.security_settings_face_enroll_introduction_info_looking;
     }
 
     @StringRes
     protected int getInfoMessageRequireEyes() {
-        return R.string.security_settings_face_enroll_introduction_info_gaze_en;
+        return R.string.security_settings_face_enroll_introduction_info_gaze;
     }
 
     @StringRes
     protected int getHowMessage() {
-        return R.string.security_settings_face_enroll_introduction_how_message_en;
+        return R.string.security_settings_face_enroll_introduction_how_message;
     }
 
     @StringRes
     protected int getInControlTitle() {
-        return R.string.security_settings_face_enroll_introduction_control_title_en;
+        return R.string.security_settings_face_enroll_introduction_control_title;
     }
 
     @StringRes
     protected int getInControlMessage() {
-        return R.string.security_settings_face_enroll_introduction_control_message_en;
+        return R.string.security_settings_face_enroll_introduction_control_message;
     }
 
     @StringRes
